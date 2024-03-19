12210d9e45b6665cc2c6e098c50bb7fdcdd2ebbd
Leele-lee <1623450919@qq.com>
2024-03-19 12:44:33 +0000

    complete with phase 5

--- bomb/bomb.asm
+++ bomb/bomb.asm
@@ -466,44 +466,44 @@ Disassembly of section .text:
   40105d:	48 83 c4 18          	add    $0x18,%rsp
   401061:	c3                   	retq   
 
-0000000000401062 <phase_5>:
-  401062:	53                   	push   %rbx
-  401063:	48 83 ec 20          	sub    $0x20,%rsp
-  401067:	48 89 fb             	mov    %rdi,%rbx
-  40106a:	64 48 8b 04 25 28 00 	mov    %fs:0x28,%rax
+0000000000401062 <phase_5>: #9on567
+  401062:	53                   	push   %rbx 
+  401063:	48 83 ec 20          	sub    $0x20,%rsp # %rsp = %rsp - 32
+  401067:	48 89 fb             	mov    %rdi,%rbx # %rbx = %rsi = input
+  40106a:	64 48 8b 04 25 28 00 	mov    %fs:0x28,%rax # %rax = canary
   401071:	00 00 
-  401073:	48 89 44 24 18       	mov    %rax,0x18(%rsp)
-  401078:	31 c0                	xor    %eax,%eax
-  40107a:	e8 9c 02 00 00       	callq  40131b <string_length>
-  40107f:	83 f8 06             	cmp    $0x6,%eax
-  401082:	74 4e                	je     4010d2 <phase_5+0x70>
-  401084:	e8 b1 03 00 00       	callq  40143a <explode_bomb>
-  401089:	eb 47                	jmp    4010d2 <phase_5+0x70>
-  40108b:	0f b6 0c 03          	movzbl (%rbx,%rax,1),%ecx
-  40108f:	88 0c 24             	mov    %cl,(%rsp)
-  401092:	48 8b 14 24          	mov    (%rsp),%rdx
-  401096:	83 e2 0f             	and    $0xf,%edx
-  401099:	0f b6 92 b0 24 40 00 	movzbl 0x4024b0(%rdx),%edx
-  4010a0:	88 54 04 10          	mov    %dl,0x10(%rsp,%rax,1)
-  4010a4:	48 83 c0 01          	add    $0x1,%rax
-  4010a8:	48 83 f8 06          	cmp    $0x6,%rax
-  4010ac:	75 dd                	jne    40108b <phase_5+0x29>
-  4010ae:	c6 44 24 16 00       	movb   $0x0,0x16(%rsp)
-  4010b3:	be 5e 24 40 00       	mov    $0x40245e,%esi
-  4010b8:	48 8d 7c 24 10       	lea    0x10(%rsp),%rdi
+  401073:	48 89 44 24 18       	mov    %rax,0x18(%rsp) # (%rsp + 24) = %rax = canary
+  401078:	31 c0                	xor    %eax,%eax # %rax = %rax ^ %rax = 0
+  40107a:	e8 9c 02 00 00       	callq  40131b <string_length> # get string length of input
+  40107f:	83 f8 06             	cmp    $0x6,%eax # compare %rax : 6
+  401082:	74 4e                	je     4010d2 <phase_5+0x70> # if %rax == 6, go on
+  401084:	e8 b1 03 00 00       	callq  40143a <explode_bomb> # else, boom!
+  401089:	eb 47                	jmp    4010d2 <phase_5+0x70> #
+  40108b:	0f b6 0c 03          	movzbl (%rbx,%rax,1),%ecx # loop, %rcx = (%rbx + %rax), the value at the i index of input, i = %rax, save in stack
+  40108f:	88 0c 24             	mov    %cl,(%rsp) # (%rsp) = %cl
+  401092:	48 8b 14 24          	mov    (%rsp),%rdx # %rdx = (%rsp) = %cl
+  401096:	83 e2 0f             	and    $0xf,%edx # %rdx = %rdx & 0x1111
+  401099:	0f b6 92 b0 24 40 00 	movzbl 0x4024b0(%rdx),%edx # %rdx = (%rdx + 0x4024b0)
+  4010a0:	88 54 04 10          	mov    %dl,0x10(%rsp,%rax,1) # (16 + %rsp + %rax) = %dl
+  4010a4:	48 83 c0 01          	add    $0x1,%rax # %rax += 1
+  4010a8:	48 83 f8 06          	cmp    $0x6,%rax # compare %rax : 6
+  4010ac:	75 dd                	jne    40108b <phase_5+0x29> # if not equal, into loop
+  4010ae:	c6 44 24 16 00       	movb   $0x0,0x16(%rsp) # else, (22 + %rsp) = 0, add the end character at end of the string
+  4010b3:	be 5e 24 40 00       	mov    $0x40245e,%esi # %rsi = 0x40245e = "flyers"
+  4010b8:	48 8d 7c 24 10       	lea    0x10(%rsp),%rdi # save the address of %rsp + 16 at %rdi
   4010bd:	e8 76 02 00 00       	callq  401338 <strings_not_equal>
-  4010c2:	85 c0                	test   %eax,%eax
+  4010c2:	85 c0                	test   %eax,%eax # if equal, array[input[i]&0xf] == target[i], go on
   4010c4:	74 13                	je     4010d9 <phase_5+0x77>
-  4010c6:	e8 6f 03 00 00       	callq  40143a <explode_bomb>
+  4010c6:	e8 6f 03 00 00       	callq  40143a <explode_bomb> # if not equal, boom!
   4010cb:	0f 1f 44 00 00       	nopl   0x0(%rax,%rax,1)
   4010d0:	eb 07                	jmp    4010d9 <phase_5+0x77>
-  4010d2:	b8 00 00 00 00       	mov    $0x0,%eax
-  4010d7:	eb b2                	jmp    40108b <phase_5+0x29>
-  4010d9:	48 8b 44 24 18       	mov    0x18(%rsp),%rax
-  4010de:	64 48 33 04 25 28 00 	xor    %fs:0x28,%rax
+  4010d2:	b8 00 00 00 00       	mov    $0x0,%eax # %rax = 0
+  4010d7:	eb b2                	jmp    40108b <phase_5+0x29> # into loop
+  4010d9:	48 8b 44 24 18       	mov    0x18(%rsp),%rax # %rax = (%rsp + 24)
+  4010de:	64 48 33 04 25 28 00 	xor    %fs:0x28,%rax # %rax = %rax ^ canary
   4010e5:	00 00 
-  4010e7:	74 05                	je     4010ee <phase_5+0x8c>
-  4010e9:	e8 42 fa ff ff       	callq  400b30 <__stack_chk_fail@plt>
+  4010e7:	74 05                	je     4010ee <phase_5+0x8c> # if equal, return
+  4010e9:	e8 42 fa ff ff       	callq  400b30 <__stack_chk_fail@plt> # else, stack has been destructed
   4010ee:	48 83 c4 20          	add    $0x20,%rsp
   4010f2:	5b                   	pop    %rbx
   4010f3:	c3                   	retq   
