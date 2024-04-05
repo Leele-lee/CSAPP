d5d456eea3b97e27d03cbd3688a4a525ddfcdecf
Leele-lee <1623450919@qq.com>
2024-04-05 08:20:21 +0000

    done with phase 5, complete attacklab!

--- attack/rtarget.asm
+++ attack/rtarget.asm
@@ -924,7 +924,7 @@ Disassembly of section .text:
   4019a0:	8d 87 48 89 c7 c3    	lea    -0x3c3876b8(%rdi),%eax
   4019a6:	c3                   	retq   
 
-00000000004019a7 <addval_219>:
+00000000004019a7 <addval_219>: # here has a gadget: 0x4019ab 58 90 c3, popq rax; nop; return
   4019a7:	8d 87 51 73 58 90    	lea    -0x6fa78caf(%rdi),%eax
   4019ad:	c3                   	retq   
 
