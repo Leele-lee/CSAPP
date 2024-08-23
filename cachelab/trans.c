312f849be88654e3fec6d599fa1e575af5794786
Leele-lee <1623450919@qq.com>
2024-08-23 08:32:01 +0000

    done 64x64 trans only for 1024 misses!

--- cachelab/trans.c
+++ cachelab/trans.c
@@ -59,130 +59,115 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
         }
     }
     //assert(is_transpose(M, N, A, B));
-
     if (N == 64) {
-        for (i = 0; i < N; i += len) {
-            // first transpose diagonal block at [i, i]
-            // borrow the next block after using diagonal 
-            // which is the first block in every block line
-            // copy the down part of A into the up part of borrow block
-            if (i == 0) j = 8; else j = 0;
+        for (j = 0; j < N; j += len) {
+            if (j == 0) i = 8; else i = 0;
+            //deal with diagonal block at [j, j]
+            //copy the down part of A into the up part of borrow block 
             for (k = 0; k < 4; k++) {
-                a0 = A[i + k + 4][i];
-                a1 = A[i + k + 4][i + 1];
-                a2 = A[i + k + 4][i + 2];
-                a3 = A[i + k + 4][i + 3];
-                a4 = A[i + k + 4][i + 4];
-                a5 = A[i + k + 4][i + 5];
-                a6 = A[i + k + 4][i + 6];
-                a7 = A[i + k + 4][i + 7];
-                B[i + k][j] = a0;
-                B[i + k][j + 1] = a1;
-                B[i + k][j + 2] = a2;
-                B[i + k][j + 3] = a3;
-                B[i + k][j + 4] = a4;
-                B[i + k][j + 5] = a5;
-                B[i + k][j + 6] = a6;
-                B[i + k][j + 7] = a7;
+                a0 = A[j + k + 4][j];
+                a1 = A[j + k + 4][j + 1];
+                a2 = A[j + k + 4][j + 2];
+                a3 = A[j + k + 4][j + 3];
+                a4 = A[j + k + 4][j + 4];
+                a5 = A[j + k + 4][j + 5];
+                a6 = A[j + k + 4][j + 6];
+                a7 = A[j + k + 4][j + 7];
+
+                B[j + k][i] = a0;
+                B[j + k][i + 1] = a1;
+                B[j + k][i + 2] = a2;
+                B[j + k][i + 3] = a3;
+                B[j + k][i + 4] = a4;
+                B[j + k][i + 5] = a5;
+                B[j + k][i + 6] = a6;
+                B[j + k][i + 7] = a7;
             }
 
-            //copy the up part 4x8 of A into the up part of B
-            for (k = 0; k < 4; k++) {
-                a0 = A[i + k][i];
-                a1 = A[i + k][i + 1];
-                a2 = A[i + k][i + 2];
-                a3 = A[i + k][i + 3];
-                a4 = A[i + k][i + 4];
-                a5 = A[i + k][i + 5];
-                a6 = A[i + k][i + 6];
-                a7 = A[i + k][i + 7];
-
-                B[i + k][i] = a0;
-                B[i + k][i + 1] = a1;
-                B[i + k][i + 2] = a2;
-                B[i + k][i + 3] = a3;
-                B[i + k][i + 4] = a4;
-                B[i + k][i + 5] = a5;
-                B[i + k][i + 6] = a6;
-                B[i + k][i + 7] = a7;
-            }
-
-            // transpose upper left and right 4x4 of B itself respectivly [i, i]
-            // need change another way to solve
-            /** for (k = i; k < i + 4; k++) {
-                for (s = k + 1; s < i + 4; s++) {
-                    a0 = B[k][s];
-                    B[k][s] = B[s][k];
-                    B[s][k] = a0;
-
-                    a0 = B[k][s + 4];
-                    B[k][s + 4] = B[s][k + 4];
-                    B[s][k + 4] = a0;
-                }
-            }**/
+            // transpose upper left and upper right of borrowing block itself respectivly [j, i] 
             for (k = 0; k < 4; k++) {
                 for (s = k + 1; s < 4; s++) {
-                    a0 = B[i + k][i + s];
-                    B[i + k][i + s] = B[i + s][i + k];
-                    B[i + s][i + k] = a0;
+                    a0 = B[j + k][i + s];
+                    B[j + k][i + s] = B[j + s][i + k];
+                    B[j + s][i + k] = a0;
 
-                    a0 = B[i + k][i + s + 4];
-                    B[i + k][i + s + 4] = B[i + s][i + k + 4];
-                    B[i + s][i + k + 4] = a0;
+                    a0 = B[j + k][i + s + 4];
+                    B[j + k][i + s + 4] = B[j + s][i + k + 4];
+                    B[j + s][i + k + 4] = a0;
                 }
             }
 
-            // transpose upper left and upper right of borrowing block itself respectivly [i, j]
-            // need double check
+            //copy the up part 4x8 of A into the up part of B 
+            for (k = 0; k < 4; k++) {
+                a0 = A[j + k][j];
+                a1 = A[j + k][j + 1];
+                a2 = A[j + k][j + 2];
+                a3 = A[j + k][j + 3];
+                a4 = A[j + k][j + 4];
+                a5 = A[j + k][j + 5];
+                a6 = A[j + k][j + 6];
+                a7 = A[j + k][j + 7];
+
+                B[j + k][j] = a0;
+                B[j + k][j + 1] = a1;
+                B[j + k][j + 2] = a2;
+                B[j + k][j + 3] = a3;
+                B[j + k][j + 4] = a4;
+                B[j + k][j + 5] = a5;
+                B[j + k][j + 6] = a6;
+                B[j + k][j + 7] = a7;
+            }
+
+            // transpose upper left and right 4x4 of B itself respectivly [j, j] 
             for (k = 0; k < 4; k++) {
                 for (s = k + 1; s < 4; s++) {
-                    a0 = B[i + k][j + s];
-                    //B[j + s][i + k] = B[i + s][j + k];
-                    B[i + k][j + s] = B[i + s][j + k];
-                    B[i + s][j + k] = a0;
+                    a0 = B[j + k][j + s];
+                    B[j + k][j + s] = B[j + s][j + k];
+                    B[j + s][j + k] = a0;
 
-                    a0 = B[i + k][j + s + 4];
-                    B[i + k][j + s + 4] = B[i + s][j + 4 + k];
-                    B[i + s][j + 4 + k] = a0;
+                    a0 = B[j + k][j + s + 4];
+                    B[j + k][j + s + 4] = B[j + s][j + k + 4];
+                    B[j + s][j + k + 4] = a0;
                 }
             }
 
-            // swap the upper right of B  and the upper left of borrowing part
+            // swaping the lower-left and upper-right
             for (k = 0; k < 4; k++) {
-                a0 = B[i + k][j];
-                a1 = B[i + k][j + 1];
-                a2 = B[i + k][j + 2];
-                a3 = B[i + k][j + 3];
+                a0 = B[j + k][j + 4];
+                a1 = B[j + k][j + 5];
+                a2 = B[j + k][j + 6];
+                a3 = B[j + k][j + 7];
 
-                B[i + k][j] = B[i + k][i + 4];
-                B[i + k][j + 1] = B[i + k][i + 5];
-                B[i + k][j + 2] = B[i + k][i + 6];
-                B[i + k][j + 3] = B[i + k][i + 7];
+                B[j + k][j + 4] = B[j + k][i];
+                B[j + k][j + 5] = B[j + k][i + 1];
+                B[j + k][j + 6] = B[j + k][i + 2];
+                B[j + k][j + 7] = B[j + k][i + 3];
 
-                B[i + k][i + 4] = a0;
-                B[i + k][i + 5] = a1;
-                B[i + k][i + 6] = a2;
-                B[i + k][i + 7] = a3;
+                B[j + k][i] = a0;
+                B[j + k][i + 1] = a1;
+                B[j + k][i + 2] = a2;
+                B[j + k][i + 3] = a3;
             }
 
-            // copy the entire upper of blocking parts into the lower part of B
+            // copy the entire upper of blocking shifting parts into the lower part of B  
             for (k = 0; k < 4; k++) {
-                B[i + k + 4][i] = B[i + k][j];
-                B[i + k + 4][i + 1] = B[i + k][j + 1];
-                B[i + k + 4][i + 2] = B[i + k][j + 2];
-                B[i + k + 4][i + 3] = B[i + k][j + 3];
-                B[i + k + 4][i + 4] = B[i + k][j + 4];
-                B[i + k + 4][i + 5] = B[i + k][j + 5];
-                B[i + k + 4][i + 6] = B[i + k][j + 6];
-                B[i + k + 4][i + 7] = B[i + k][j + 7];
+                B[j + k + 4][j] = B[j + k][i];
+                B[j + k + 4][j + 1] = B[j + k][i + 1];
+                B[j + k + 4][j + 2] = B[j + k][i + 2];
+                B[j + k + 4][j + 3] = B[j + k][i + 3];
+                B[j + k + 4][j + 4] = B[j + k][i + 4];
+                B[j + k + 4][j + 5] = B[j + k][i + 5];
+                B[j + k + 4][j + 6] = B[j + k][i + 6];
+                B[j + k + 4][j + 7] = B[j + k][i + 7];
             }
 
-            // processing off-diagonal blocks
-            for (j = 0; j < M; j += len) {
+            //for non-diagnoal block A and B is not correspond, A is reading cloumn-first
+            // B is row-first, A start with [j,i] is corresponding B with [i, j]
+            for (i = 0; i < M; i += len) {
                 if (i == j) {
                     continue;
-                } else { 
-                    // copy transposing the upper left 4x4 part and the upper right 4x4 part of A into B
+                } else {
+                    // copy transposing the upper left and right 4x4 part from A to B
                     for (k = 0; k < 4; k++) {
                         a0 = A[i + k][j];
                         a1 = A[i + k][j + 1];
@@ -193,54 +178,56 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
                         a6 = A[i + k][j + 6];
                         a7 = A[i + k][j + 7];
 
-                        B[i][j + k] = a0;
-                        B[i + 1][j + k] = a1;
-                        B[i + 2][j + k] = a2;
-                        B[i + 3][j + k] = a3;
+                        B[j][i + k] = a0;
+                        B[j + 1][i + k] = a1;
+                        B[j + 2][i + k] = a2;
+                        B[j + 3][i + k] = a3;
 
-                        B[i][j + k + 4] = a4;
-                        B[i + 1][j + k + 4] = a5;
-                        B[i + 2][j + k + 4] = a6;
-                        B[i + 3][j + k + 4] = a7;
+                        B[j][i + k + 4] = a4;
+                        B[j + 1][i + k + 4] = a5;
+                        B[j + 2][i + k + 4] = a6;
+                        B[j + 3][i + k + 4] = a7;
                     }
-
-                    // taking transpose of lower-left 4x4 and store to upper-right 4x4, 
-                    // and move upper-right 4x4 to lower-left 4x4
-				    for (k = 0; k < 4; k++) {
+                    // taking transpose of lower-left 4x4 of A and store to upper-right 4x4 of B, 
+                    // and move upper-right 4x4 of B to lower-left 4x4 of B
+                    for (k = 0; k < 4; k++) {
                         a0 = A[i + 4][j + k];
                         a1 = A[i + 5][j + k];
                         a2 = A[i + 6][j + k];
                         a3 = A[i + 7][j + k];
 
-                        a4 = B[i + k][j + 4];
-                        a5 = B[i + k][j + 5];
-                        a6 = B[i + k][j + 6];
-                        a7 = B[i + k][j + 7];
+                        a4 = B[j + k][i + 4];
+                        a5 = B[j + k][i + 5];
+                        a6 = B[j + k][i + 6];
+                        a7 = B[j + k][i + 7];
 
-                        B[i + k][j + 4] = a0;
-                        B[i + k][j + 5] = a1;
-                        B[i + k][j + 6] = a2;
-                        B[i + k][j + 7] = a3;
+                        B[j + k][i + 4] = a0;
+                        B[j + k][i + 5] = a1;
+                        B[j + k][i + 6] = a2;
+                        B[j + k][i + 7] = a3;
 
-                        B[i + k + 4][j] = a4;
-                        B[i + k + 4][j + 1] = a5;
-                        B[i + k + 4][j + 2] = a6;
-                        B[i + k + 4][j + 3] = a7;
+                        B[j + k + 4][i] = a4;
+                        B[j + k + 4][i + 1] = a5;
+                        B[j + k + 4][i + 2] = a6;
+                        B[j + k + 4][i + 3] = a7;
                     }
 
-                    // taking transpose of lower-right 4x4 of A and store to lower-right 4x4 of B
+                    // taking transpose of lower-right 4x4 of A to B
                     for (k = 0; k < 4; k++) {
                         a0 = A[i + 4 + k][j + 4];
                         a1 = A[i + 4 + k][j + 5];
                         a2 = A[i + 4 + k][j + 6];
                         a3 = A[i + 4 + k][j + 7];
 
-                        B[i + 4][j + 4 + k] = a0;
-                        B[i + 5][j + 4 + k] = a1;
-                        B[i + 6][j + 4 + k] = a2;
-                        B[i + 7][j + 4 + k] = a3;
+                        B[j + 4][i + k + 4] = a0;
+                        B[j + 5][i + k + 4] = a1;
+                        B[j + 6][i + k + 4] = a2;
+                        B[j + 7][i + k + 4] = a3;
+
                     }
+
                 }
+
             }
         }
     }
